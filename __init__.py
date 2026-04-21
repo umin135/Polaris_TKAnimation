e155b859ca459c45c3d5c501d83d7ba82fd25bfd
Koo Minu <44253739+umin135@users.noreply.github.com>
2026-04-22 00:36:10 +0900

    1.3

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Polaris TKAnimation",
     "author": "UMIN",
-    "version": (1, 2, 0),
+    "version": (1, 3, 0),
     "blender": (3, 6, 0),
     "location": "File > Import-Export",
     "description": "Import/Export Polaris (Tekken 8) animation data.",
